42ee9edc29e28a912c954cdf201fd4f2f76e5fce
akalanka <akalankapathirage22@gmail.com>
2024-05-26 18:57:42 +0530

    developed componets upto step3 + commented postgre sql code temporaily

--- packages/nextjs/app/api/list-assets/route.ts
+++ packages/nextjs/app/api/list-assets/route.ts
@@ -1,56 +1,60 @@
 import { sql } from "@vercel/postgres";
+import { writeFileSync } from "fs";
 
 export async function POST(request: Request) {
   const reqBody = await request.json();
   console.log({ reqBody });
-  try {
-    // Parse and validate the input data
-    const { address, price, fractions, photo, description } = reqBody.formData;
-    const { connectedWalletAddress } = reqBody;
+  // Save req body to a JSON file
+  writeFileSync("reqBody.json", JSON.stringify(reqBody));
+  return Response.json({ message: "Property listed successfully" }, { status: 201 });
+  // try {
+  //   // Parse and validate the input data
+  //   const { address, price, fractions, photo, description } = reqBody.formData;
+  //   const { connectedWalletAddress } = reqBody;
 
-    if (!address || !price || !fractions) {
-      return Response.json(
-        { message: "Please fill in all required fields." },
-        {
-          status: 400,
-        },
-      );
-    }
+  //   if (!address || !price || !fractions) {
+  //     return Response.json(
+  //       { message: "Please fill in all required fields." },
+  //       {
+  //         status: 400,
+  //       },
+  //     );
+  //   }
 
-    // Connect to the database
-    await sql`CREATE TABLE IF NOT EXISTS properties (
-      id SERIAL PRIMARY KEY,
-      connected_wallet_address TEXT NOT NULL,
-      address TEXT NOT NULL,
-      total_value NUMERIC NOT NULL,
-      fractions_count INT NOT NULL,
-      photo TEXT,
-      description TEXT NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )`;
+  //   // Connect to the database
+  //   await sql`CREATE TABLE IF NOT EXISTS properties (
+  //     id SERIAL PRIMARY KEY,
+  //     connected_wallet_address TEXT NOT NULL,
+  //     address TEXT NOT NULL,
+  //     total_value NUMERIC NOT NULL,
+  //     fractions_count INT NOT NULL,
+  //     photo TEXT,
+  //     description TEXT NOT NULL,
+  //     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  //   )`;
 
-    // Insert the property into the database
-    const result = await sql`
-      INSERT INTO properties (connected_wallet_address, address, total_value, fractions_count, photo, description)
-      VALUES (${connectedWalletAddress} ${address}, ${price}, ${fractions}, ${photo}, ${description})
-      RETURNING *
-    `;
+  //   // Insert the property into the database
+  //   const result = await sql`
+  //     INSERT INTO properties (connected_wallet_address, address, total_value, fractions_count, photo, description)
+  //     VALUES (${connectedWalletAddress} ${address}, ${price}, ${fractions}, ${photo}, ${description})
+  //     RETURNING *
+  //   `;
 
-    const newProperty = result.rows[0];
+  //   const newProperty = result.rows[0];
 
-    return Response.json(
-      { message: "Property listed successfully", property: newProperty },
-      {
-        status: 201,
-      },
-    );
-  } catch (error) {
-    console.error(error);
-    return Response.json(
-      { message: "Internal server error" },
-      {
-        status: 500,
-      },
-    );
-  }
+  //   return Response.json(
+  //     { message: "Property listed successfully", property: newProperty },
+  //     {
+  //       status: 201,
+  //     },
+  //   );
+  // } catch (error) {
+  //   console.error(error);
+  //   return Response.json(
+  //     { message: "Internal server error" },
+  //     {
+  //       status: 500,
+  //     },
+  //   );
+  // }
 }
--- packages/nextjs/app/list-assets/real-estate-listing/index.tsx
+++ packages/nextjs/app/list-assets/real-estate-listing/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { useAccount } from "wagmi";
 
@@ -127,25 +127,156 @@ const ListingForm = () => {
   );
 };
 
+const OwnershipVerification = () => {
+  const [verifiers, setVerifiers] = useState<any>([]);
+  const [selectedVerifier, setSelectedVerifier] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    // Fetch pre-vetted verifiers from your backend or hardcode for now
+    setVerifiers(["Land Registry of Asgard", "A Titlte Search Company", "Verifier C"]);
+  }, []);
+
+  const handleVerifierChange = (e: any) => {
+    setSelectedVerifier(e.target.value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setSubmitting(true);
+    console.log("Selected Verifier:", selectedVerifier);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
+    // Call your backend to add the selected verifier to the property. Verifier should get notified from the backend.
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto flex">
+      <select
+        id="verifier"
+        value={selectedVerifier}
+        onChange={handleVerifierChange}
+        required
+        className="select select-primary w-full max-w-xs"
+      >
+        <option value="" disabled selected>
+          Select a Verifier
+        </option>
+        {verifiers.map((verifier: any, index: number) => (
+          <option key={index} value={verifier}>
+            {verifier}
+          </option>
+        ))}
+      </select>
+      <button type="submit" className="ml-5 btn-link">
+        {submitting ? "Submitting" : "Submit"}
+      </button>
+    </form>
+  );
+};
+
+const IncludeGurantees = () => {
+  const [gurantors, setGurantors] = useState<any>([]);
+  const [selectedGurantor, setSelectedGurantor] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    // Fetch pre-vetted Gurantors from your backend or hardcode for now
+    setGurantors(["Asset Holder Capital", "Some Gurantor Company", "Gurantor C"]);
+  }, []);
+
+  const handleGurantorChange = (e: any) => {
+    setSelectedGurantor(e.target.value);
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setSubmitting(true);
+    console.log({ selectedGurantor });
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
+    // Call your backend to add the selected verifier to the property. Verifier should get notified from the backend.
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit}>
+        <label className="block font-bold mb-2">Select a Gurantor</label>
+        <div className="flex">
+          <select
+            id="gurantor"
+            value={selectedGurantor}
+            onChange={handleGurantorChange}
+            required
+            className="select select-primary w-full max-w-xs"
+          >
+            <option value="" disabled selected>
+              Select a Gurantor
+            </option>
+            {gurantors.map((gurantor: any, index: number) => (
+              <option key={index} value={gurantor}>
+                {gurantor}
+              </option>
+            ))}
+          </select>
+          <button type="submit" className="ml-5 btn-link">
+            {submitting ? "Submitting" : "Submit"}
+          </button>
+        </div>
+      </form>
+      <div className="mt-10 collapse collapse-open">
+        <input type="checkbox" className="peer" />
+        <div className="collapse-title bg-secondary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+          Or Go to this link and add collataral ( by yourself or via thid party )
+        </div>
+        <div className="collapse-content bg-primary text-primary-content peer-checked:bg-primary peer-checked:text-secondary-content">
+          <a href="https://docs.attest.org/docs/tutorials/create-a-schema" className="link" target="_blank">
+            https://docs.attest.org/docs/tutorials/create-a-schema
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const RealEstateListing = () => {
   const steps = [
     { number: 1, title: "Listing Form" },
     { number: 2, title: "Add ownership Verfications" },
-    { number: 3, title: "Sign TOS" },
-    { number: 4, title: "Add Gurantees" },
+    { number: 3, title: "Add Gurantees" },
+    { number: 4, title: "Sign TOS" },
     { number: 5, title: "Review and List" },
   ];
   const [step, setStep] = useState(1);
+
+  const handleStepChange = (clickedStep: number) => {
+    if (clickedStep >= 4) {
+      // Call your backend to check if the atleast one gurantor has been added.
+      const guranterAdded = false;
+      if (!guranterAdded) {
+        alert("Please add a gurantor to proceed");
+        return;
+      }
+    }
+    setStep(clickedStep);
+  };
+
   return (
     <div className="flex flex-col">
       <ul className="steps mb-10">
         {steps.map(({ number, title }) => (
-          <li className={clsx("step", { "step-primary": step === number })} onClick={_ => setStep(number)}>
+          <li className={clsx("step", { "step-primary": step === number })} onClick={() => handleStepChange(number)}>
             {title}
           </li>
         ))}
       </ul>
       {step === 1 && <ListingForm />}
+      {step === 2 && <OwnershipVerification />}
+      {step === 3 && <IncludeGurantees />}
     </div>
   );
 };
