eda316fae1bb7229513e8e5b2f0871c6d46a7fe9
akalanka <akalankapathirage22@gmail.com>
2024-05-26 09:49:34 +0530

    metadata change

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,8 +5,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "LinkReal",
+  description: "The Ultimate RWA Platform",
 });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
